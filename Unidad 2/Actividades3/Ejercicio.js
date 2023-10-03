7f6e1ff781d8e11d93cebf6d3a584e6d8edbd4c6
adrperser2@alu.edu.gva.es <adrperser2@alu.edu.gva>
2023-10-03 16:45:21 +0200

    Actividades 3

---
+++ Unidad 2/Actividades3/Ejercicio.js
@@ -0,0 +1,5 @@
+let x = Math.floor(Math.random() * 256);
+let y = Math.floor(Math.random() * 256);
+let z = Math.floor(Math.random() * 256);
+let rgb = 'rgb(' + x + ',' + y + ',' + z + ')';
+document.body.style.backgroundColor = rgb;
