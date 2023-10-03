f83d85d394dc4be1776c1e21eca89516dc9e652d
adrperser2@alu.edu.gva.es <adrperser2@alu.edu.gva>
2023-10-03 16:52:16 +0200

    Actividad 4

---
+++ Unidad 2/Actividades4/Ejercicio.js
@@ -0,0 +1,11 @@
+let num = prompt("Ingresa un numero:");
+
+if (!isNaN(num) && num !== null && num !== "") {
+
+    alert("Bien, has ingresado un numero");
+
+} else {
+
+    alert("No has ingresado un numero");
+
+}
